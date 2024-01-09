6b83b71588ca6aaebcdc6dc17f3bdaf53f8b5a12
Stephen Dawson <stephendawson@protonmail.com>
2024-01-09 14:37:38 +0000

    add sorting by category

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
+
+# Create your views here.
 
 
 def all_products(request):
@@ -20,7 +23,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -56,7 +60,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show selected product """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
