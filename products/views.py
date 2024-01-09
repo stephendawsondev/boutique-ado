d98f9e8a34f9478fa07fec665e85c570f3fc2bcb
Stephen Dawson <stephendawson@protonmail.com>
2024-01-09 13:53:37 +0000

    fix missing product details page

--- products/views.py
+++ products/views.py
@@ -23,4 +23,4 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
