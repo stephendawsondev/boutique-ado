18d07c6ed3c981ad375a120d762dab33a44675eb
Stephen Dawson <stephendawson@protonmail.com>
2024-01-16 12:04:12 +0000

    update checkout admin, settings.py, edit product and views

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,27 +3,28 @@ from .models import Order, OrderLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
-    """ 
-    Allows us to add and edit line items in the admin right from inside
-    the order model.
-    """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
+
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'original_bag', 'stripe_pid')
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
-              'street_address1', 'street_address2', 'town_or_city',
-              'postcode', 'county', 'delivery_cost', 'order_total',
-              'grand_total', 'original_bag', 'stripe_pid')
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
-    list_display = ('order_number', 'date', 'full_name', 'delivery_cost',
-                    'order_total', 'grand_total')
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
 
     ordering = ('-date',)
 
